ec661eb4be6b52b014369c2bf95fc5cc1791ffa7
okn-yu <haruka.koishikawa@gmail.com>
2018-11-08 23:59:42 +0900

    updated.

---
+++ src/com/company/util/Pair.java
@@ -0,0 +1,30 @@
+package com.company.util;
+
+import java.util.Objects;
+
+public final class Pair<S, T> {
+
+    private final S firstElm;
+    private final T secElm;
+
+    public Pair(S firstElm, T secElm) {
+        this.firstElm = firstElm;
+        this.secElm = secElm;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Pair) {
+            Pair pair = (Pair) obj;
+            return this.firstElm == pair.firstElm && this.secElm == pair.secElm;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstElm, secElm);
+    }
+
+}
